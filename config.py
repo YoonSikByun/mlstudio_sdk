224b940d54fbe191dfa49a490ae1ea98ce1afa80
yunsikpyun@gamil.com <yunsikpyun@gamil.com>
2024-06-18 15:40:08 +0000

    add
    add

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import jsons
-from common import SingletonType
+from mlstudio_sdk.common import SingletonType
 
 config_path = '/opt/mlstudio/config/mlstudio-config.json'
 
@@ -12,7 +12,7 @@ class Config(object, metaclass=SingletonType) :
         with open(config_path, 'r') as f :
             self.config = jsons.loads(f.read())
 
-    def get_db_mlflow_url(self) :
+    def get_mlflow_tracking_uri(self) :
         ip = self.config['db']['cluster']['ip']
         port = self.config['db']['cluster']['port']
         user_id = self.config['db']['user']['mlflow']['id']
@@ -20,8 +20,12 @@ class Config(object, metaclass=SingletonType) :
         dbname = self.config['db']['database']['mlflow']['tracking']
 
         return f"postgresql://{user_id}:{user_pwd}@{ip}:{port}/{dbname}"
-            
+
+    def get_mlflow_artifact_url(self) :
+        return self.config['mlflow']['artifact_root']
+
 if __name__ == '__main__':
     config = Config()
     print(config.config)
-    print(config.get_db_mlflow_url())
+    print(config.get_mlflow_tracking_uri())
+    print(config.get_mlflow_artifact_url())
